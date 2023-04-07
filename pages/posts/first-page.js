29f759030a111095853af0f8d515e39ecd6f5087
heewon <hwyang0627@hanyang.ac.kr>
2023-04-07 13:21:15 +0900

    commit

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function FirstPost() {
   return (
     <>
-      <h1>Heewon Yang</h1>
-      <p>Age: 19</p>
+      <h1 style={{color: 'Blue'}}>Heewon Yang</h1>
+      <p>Age: 21</p>
       <p>Hobby: Traveling abroad</p>
       <button>
         <Link href="/">Back to home</Link>
